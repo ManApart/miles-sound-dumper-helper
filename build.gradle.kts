d8c9c41946b53b6f4f0b41268bb01fe57ca05404
ManApart <raustinkucera@gmail.com>
2019-12-15 09:59:12 -0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "org.rak"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    idea
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit:junit:4.12")
+    testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.2.51")
+    testCompile("org.jetbrains.kotlin:kotlin-test:1.3.50")
+}
---
+++ msdHelper.kts
@@ -0,0 +1,7 @@
+fun hello(): String {
+    return "ok"
+}
+
+println(hello())
+
+println("do the thing")
